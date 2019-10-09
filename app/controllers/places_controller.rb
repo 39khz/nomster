134aaf18a461570a857bed3fad5df2540a8a3593
andy <andy@goomy.dev>
2019-10-09 02:19:40 +0000

    added validation on :name, styled validation error messages

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -11,7 +11,11 @@ class PlacesController < ApplicationController
 
   def create
     current_user.places.create(place_params)
-    redirect_to root_path
+    if @place.valid?
+        redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def show
@@ -33,10 +37,13 @@ class PlacesController < ApplicationController
       end
 
     @place.update_attributes(place_params)
-    redirect_to root_path
+    if @place.valid?
+      redirect_to root_path
+    else
+      render :edit, status: :unprocessable_entity
+    end
   end
-
-  def destroy
+    def destroy
     @place = Place.find(params[:id])
     if @place.user != current_user
       return render plain: "Not Allowed", status: :forbidden
